fd496a8a9784d906b4f50eb0cf15778f55985182
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2023-01-19 10:41:15 -0500

    fixing meta tags according to docs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,18 +15,15 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='en'>  
+      <Html lang='en'className="bg-gray-300">
         <Head>
-          <meta charSet='utf-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <meta name='theme-color' content='#000000' />
         </Head>
         <body>
           <Main />
           <NextScript />
+         
         </body>
       </Html>
-
     );
   }
 }
