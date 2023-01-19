bebd1ef06be85de03b46dea2e28e7a1edd97282c
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2023-01-19 10:39:01 -0500

    has to be src with relative path for image cmpnt

--- src/components/layout/HeroHome.tsx
+++ src/components/layout/HeroHome.tsx
@@ -54,7 +54,7 @@ function HeroHome() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <Image
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={VsImage}
+          src={'/public/images/red-v-blue.jpg'}
           height={500}
           width={500}
           alt="Hero Image"
