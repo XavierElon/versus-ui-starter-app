0af4e201b1728b534bd84bb9b15b02b88184bbb5
Xavier <xavierh93@hotmail.com>
2023-01-23 17:27:16 -0500

    add logo

--- src/components/layout/HeroHome.tsx
+++ src/components/layout/HeroHome.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
+import VSLogo from '../../public/images/red-v-blue.jpg'
 
 function HeroHome() {
   return (
@@ -54,7 +55,7 @@ function HeroHome() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <Image
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={'/public/images/red-v-blue.jpg'}
+          src={VSLogo}
           height={500}
           width={500}
           alt="Hero Image"
