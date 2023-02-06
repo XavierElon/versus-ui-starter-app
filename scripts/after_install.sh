f103c207e41d464ed1b7baa57b8ff24cee696a97
Xavier <xavierh93@hotmail.com>
2023-02-06 09:00:59 -0700

    update scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,6 @@
 cd /ec2-user/home/versus/versus-ui-starter-app
 touch test1.txt
 echo 'test' >> test1.txt
+nvm install 16
 npm i -g pm2
 yarn install
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,6 +3,7 @@
 cd /ec2-user/home/versus/versus-ui-start-app
 touch test3.txt
 echo 'test' >> test3.txt
+nvm install 16
 npm install -g pm2
 pm2 stop "versus-app"
 pm2 delete "versus-app"
