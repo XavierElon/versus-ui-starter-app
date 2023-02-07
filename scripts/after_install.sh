e05fbdb9bd8125d283a8aa2171cdb1d4a9a69392
Xavier <xavierh93@hotmail.com>
2023-02-06 20:51:37 -0700

    update after_install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,11 @@
 
 cd /ec2-user/home/versus/versus-ui-starter-app
 touch test1.txt
+echo 'hello world'
 echo 'test' >> test1.txt
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm install 16
 npm i -g pm2
+npm i -g yarn
 yarn install
