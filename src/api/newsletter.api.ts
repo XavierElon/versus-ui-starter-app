3dbdd1143dbec375dc6d601e91d77126bb4c4547
Xavier <xavierh93@hotmail.com>
2023-02-23 19:14:33 -0500

    create NewsletterCard component

--- src/api/newsletter.api.ts
+++ src/api/newsletter.api.ts
@@ -1,19 +1,18 @@
 import axios from 'axios'
 
 export const createNewsletterUser = async (email: string) => {
-    console.log('here')
-    console.log(process.env.NEXT_PUBLIC_NEWSLETTER_URL)
-    setTimeout(() => {
-        console.log('timeing')
-    }, 3000)
-    // try {
-    //     axios.post(process.env.NEWSLETTER_URL, {
-    //         email: email,
-    //         subscribed: true
-    //     })
-    //     console.log('posted')
-    // } catch (error) {
-    //     console.log(`${error}`)
-    // }
-        
+  console.log('here')
+  console.log(process.env.NEXT_PUBLIC_NEWSLETTER_URL)
+  setTimeout(() => {
+    console.log('timeing')
+  }, 3000)
+  // try {
+  //     axios.post(process.env.NEWSLETTER_URL, {
+  //         email: email,
+  //         subscribed: true
+  //     })
+  //     console.log('posted')
+  // } catch (error) {
+  //     console.log(`${error}`)
+  // }
 }
