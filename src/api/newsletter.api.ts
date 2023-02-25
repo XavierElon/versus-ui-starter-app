4203a25bec8e97bdea2bebeb12a2013035d690c8
Xavier <xavierh93@hotmail.com>
2023-02-25 12:03:54 -0500

    chagne api call

---
+++ src/api/newsletter.api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+interface NewsletterUserData {
+    email: string,
+    subscribed: boolean
+}
+
+export const postUser = async (props: NewsletterUserData) => {
+    const { email, subscribed } = props
+    const url = process.env.NEXT_PUBLIC_LOCAL_HOST! + 420 + '/newsletter'
+    console.log(url)
+    console.log(email)
+    console.log(subscribed)
+      try {
+        axios.post(url, {
+          email: email,
+          subscribed: subscribed
+        })
+        console.log('posted')
+      } catch (error) {
+        console.log(`${error}`)
+      }
+    
+
+
+}
