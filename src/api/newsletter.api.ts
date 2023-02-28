554b44c6a5bfee270add9136b55258b9ca66a03a
Xavier <xavierh93@hotmail.com>
2023-02-28 18:44:30 -0500

    fix post error catch

--- src/api/newsletter.api.ts
+++ src/api/newsletter.api.ts
@@ -1,18 +1,20 @@
 import { NewsletterUserData } from '@/models/interfaces'
 import axios from 'axios'
 
-export const createNewsletterUser = async (newletterForm: NewsletterUserData) => {
+export const createNewsletterUser = async (
+  newletterForm: NewsletterUserData
+) => {
   const { email, subscribed } = newletterForm
   const url = process.env.NEXT_PUBLIC_LOCAL_HOST! + 420 + '/newsletter'
   console.log(url)
   console.log(email)
   try {
-    axios.post(url, {
-      email: email,
-      subscribed: subscribed
-    }).catch((error) => console.log(error))
+   await axios
+      .post(url, {
+        email: email,
+        subscribed: subscribed
+      })
   } catch (error) {
-    console.log('failed')
-    console.log(`${error}`)
+    console.error(`${error}`)
   }
 }
